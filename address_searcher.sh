1cc0c1eda2b2425b3e1d544f5d86284e3f8bcb41
herrBez <mirko.bez@studenti.unipd.it>
2018-01-22 23:07:06 +0100

    Output repository url and file + add other formats Add file format.json that contains the configuration to use to check the addresses for the different cryptocurrencies + Fix URL output

--- address_searcher.sh
+++ address_searcher.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
 
 
-readonly search_term=(BTC LTC NMC DOGE XMR ETH)
+readonly BASE_OUTPUT_FILE=/tmp/output
+readonly BASE_RESULT_FILE=/tmp/result
+readonly FORMAT_FILE=format.json
 
 main() {
-    local readonly TMP_OUTPUT_FILE=/tmp/output.json
-    local readonly TMP_RESULT_FILE=/tmp/result.json
+    local readonly FORMAT_INDEX=$(cat $FORMAT_FILE | jq 'keys' | jq '.[]')
     
-    # 0..49 are the allowed result page for the searchcode api
-    for p in {0..49}; do
-        # Download the json file containing the infos
-        curl -s -o $TMP_OUTPUT_FILE https://searchcode.com/api/codesearch_I/?q=BTC+donation&p=$p&per_page=100&lan=122&lan=118&lan=184&loc=5
+    for f in $FORMAT_INDEX; do
+        cat $FORMAT_FILE | jq ".[$f].name"
+        local readonly REGEXP=$(cat $FORMAT_FILE | jq ".[$f].wallet_regexp")
+        local readonly SYMBOL=$(cat $FORMAT_FILE | jq --raw-output ".[$f].symbol")
         
-        # Extract the "result" key from the json file and store it in $TMP_RESULT_FILE
-        cat $TMP_OUTPUT_FILE | jq '.' |  jq '.["results"]' > $TMP_RESULT_FILE
+        local readonly RESULT_FILE="$BASE_RESULT_FILE-$SYMBOL.json"
+        local readonly SEARCH_STRING="$SYMBOL+donation"
+        
+        # 0..49 are the allowed result page for the searchcode api
+        for p in {0..49}; do
+            local readonly OUTPUT_FILE="$BASE_OUTPUT_FILE-$SYMBOL-$p.json"
+            local readonly RESULT_FILE="$BASE_RESULT_FILE-$SYMBOL-$p.json"
             
-        # Extract the indices of the result_file
-        local readonly KEYS=$(cat $TMP_RESULT_FILE | jq 'keys' | jq '.[]')
-        
-        
-        for k in $KEYS; do
-            local readonly TMP=$(cat $TMP_RESULT_FILE | jq ".[$k]")
+            # Download the json file containing the infos
+            curl -s -o $OUTPUT_FILE https://searchcode.com/api/codesearch_I/?q=$SEARCH_STRING&p=$p&per_page=100&loc=5
             
-            echo $TMP | jq '.' | egrep -o '[13][0-9A-Za-z]{24,35}'
-            if [ "$?" == "0" ]; then # a match is found
-                local readonly REPO=$(echo $TMP | jq ".repo")
-                local readonly FILENAME=$(echo $TMP | jq ".filename")
-                local readonly URL=$REPO\/$FILENAME
-                echo $URL
-            fi
-            echo "==="
+            #~ # Extract the "result" key from the json file and store it in $TMP_RESULT_FILE
+            cat $OUTPUT_FILE | jq '.' |  jq '.["results"]' > $RESULT_FILE
+            
+            #~ # Extract the indices of the result_file
+            local readonly KEYS=$(cat $RESULT_FILE | jq 'keys' | jq '.[]')
+            
+            for k in $KEYS; do
+                local readonly TMP=$(cat $RESULT_FILE | jq ".[$k]")
+                
+                
+                
+                local readonly MATCH=$(echo $TMP | jq '.' | egrep -o "$REGEXP")
+                #~ echo $TMP | jq '.' | egrep -o "$REGEXP"
+                if [ "$MATCH" != "" ]; then # a match is found
+                    echo "==="
+                    echo $TMP | jq "."
+                    echo $MATCH
+                    local readonly REPOSITORY=$(echo $TMP | jq --raw-output ".repo" | sed 's/\.[^.]*$//')
+                    local readonly RAW_REPOSITORY=$(echo $REPOSITORY | sed 's/github.com/raw.githubusercontent.com/')
+                    local readonly DIRECTORY=$(echo $TMP | jq --raw-output ".location")
+                    local readonly FILENAME=$(echo $TMP | jq --raw-output ".filename")
+                    local URL=""
+                    if [ "$DIRECTORY" == "" ]; then
+                        URL=$RAW_REPOSITORY/master/$FILENAME
+                    else
+                        URL=$RAW_REPOSITORY/master/$DIRECTORY/$FILENAME
+                    fi
+                    echo $URL
+                    echo "==="
+                fi
+            done
         done
     done
 }
