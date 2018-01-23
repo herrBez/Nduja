2c1f58fcbada97d0fd0055ce19baa54332dd9d9e
herrBez <mirko.bez@studenti.unipd.it>
2018-01-23 17:34:41 +0100

    Add pretty-json output with WALLET_LIST,USERNAME,REPO

--- address_searcher.sh
+++ address_searcher.sh
@@ -5,6 +5,7 @@ readonly BASE_OUTPUT_FILE=/tmp/output
 readonly BASE_RESULT_FILE=/tmp/result
 readonly FORMAT_FILE=format.json
 
+
 main() {
     local readonly FORMAT_INDEX=$(cat $FORMAT_FILE | jq 'keys' | jq '.[]')
     local counter=0
@@ -12,49 +13,60 @@ main() {
     for f in $FORMAT_INDEX; do
         cat $FORMAT_FILE | jq ".[$f].name"
         local readonly REGEXP=$(cat $FORMAT_FILE | jq --raw-output ".[$f].wallet_regexp")
-        echo $REGEXP
-        sleep 1
+        local readonly NAME=$(cat $FORMAT_FILE | jq --raw-output ".[$f].name")
         local readonly SYMBOL=$(cat $FORMAT_FILE | jq --raw-output ".[$f].symbol")
-
-        local readonly RESULT_FILE="$BASE_RESULT_FILE-$SYMBOL.json"
-        local readonly SEARCH_STRING="$SYMBOL+Donation"
+        local readonly SEARCH_STRING="$SYMBOL+Donation+$NAME"
+        
+        echo "$SEARCH_STRING"
 
         # 0..49 are the allowed result page for the searchcode api
         for p in {0..49}; do
             echo "page $p"
             local readonly OUTPUT_FILE="$BASE_OUTPUT_FILE-$SYMBOL-$p.json"
             local readonly RESULT_FILE="$BASE_RESULT_FILE-$SYMBOL-$p.json"
-
+            
+            touch $OUTPUT_FILE
+            touch $RESULT_FILE
+            
             # Download the json file containing the infos
-            curl -s -o $OUTPUT_FILE https://searchcode.com/api/codesearch_I/?q=${SEARCH_STRING}&p=$p&per_page=100&loc=1000
-            #~ # Extract the "result" key from the json file and store it in $TMP_RESULT_FILE
+            curl -s -o $OUTPUT_FILE https://searchcode.com/api/codesearch_I/?q=${SEARCH_STRING}&p=${p}&per_page=100&loc=0
+            # Extract the "result" key from the json file and store it in $TMP_RESULT_FILE
             cat $OUTPUT_FILE | jq '.' |  jq '.["results"]' > $RESULT_FILE
 
-            #~ # Extract the indices of the result_file
+            # Extract the indices of the result_file
             local readonly KEYS=$(cat $RESULT_FILE | jq 'keys' | jq '.[]')
 
             for k in $KEYS; do
                 local readonly TMP=$(cat $RESULT_FILE | jq ".[$k]")
-                local readonly MATCH=$(echo $TMP | jq "." | jq ".lines" | egrep -o "$REGEXP")
-                #~ echo $TMP | jq '.' | egrep -o "$REGEXP"
-                if [ "$MATCH" != "" ]; then # a match is found
-                    echo "====="
-                    #echo $TMP | jq "."
-                    echo $MATCH
+                local readonly MATCH=$(echo $TMP | jq "." | jq --raw-output ".lines" | egrep -o "$REGEXP")
+                
+                if [ "$MATCH" != "" ]; then # at least a match is found
                     local readonly REPOSITORY=$(echo $TMP | jq --raw-output ".repo" | sed 's/\.[^.]*$//')
                     local readonly RAW_REPOSITORY=$(echo $REPOSITORY | sed 's/github.com/raw.githubusercontent.com/')
                     local readonly DIRECTORY=$(echo $TMP | jq --raw-output ".location")
                     local readonly FILENAME=$(echo $TMP | jq --raw-output ".filename")
                     local URL=""
-                    if [ "$DIRECTORY" == "" ]; then
-                        URL=$RAW_REPOSITORY/master/$FILENAME
+                    if [ "$DIRECTORY" == "" ] || [ "$DIRECTORY" == "/" ]; then
+                        URL=$REPOSITORY/$FILENAME
+                        RAW_URL=$RAW_REPOSITORY/master/$FILENAME
                     else
-                        URL=$RAW_REPOSITORY/master/$DIRECTORY/$FILENAME
+                        URL=$REPOSITORY/$DIRECTORY/$FILENAME
+                        RAW_URL=$RAW_REPOSITORY/master/$DIRECTORY/$FILENAME
                     fi
-                    echo "$URL $counter"
-
+                    
+                    local readonly WALLETS=$(printf "[$MATCH]" | tr '\n' ',')
+                    local readonly HOST=$(printf "$REPOSITORY" | egrep -o "https?://[^/]*" | sed -r "s/https?:\/\///")
+                    local readonly USERNAME=$(printf "$REPOSITORY" | sed -r "s/https?:\/\/[^/]*\/([^/]*)\/.*/\1/")
+                    echo "{\
+                    \"username\": \"$USERNAME\",\
+                    \"host\": \"$HOST\", \
+                    \"symbol\" : \"$SYMBOL\", \
+                    \"url\": \"$URL\",\
+                    \"known_raw_url\": \"RAW_URL\",\
+                    \"pathToFile\": \"$DIRECTORY/$FILENAME\",\
+                    \"wallet\": \"$WALLETS\"\
+                    }" | jq "."
                     counter=$((counter+1))
-                    echo "====="
                 fi
             done
         done
