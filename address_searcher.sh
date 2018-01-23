c3ff9fe388428225c8186e7b6a2f4cd338f5fc24
herrBez <mirko.bez@studenti.unipd.it>
2018-01-23 12:03:42 +0100

    Match only on "lines" and removed quotes from REGEXP

--- address_searcher.sh
+++ address_searcher.sh
@@ -7,39 +7,39 @@ readonly FORMAT_FILE=format.json
 
 main() {
     local readonly FORMAT_INDEX=$(cat $FORMAT_FILE | jq 'keys' | jq '.[]')
-    
+    local counter=0
+
     for f in $FORMAT_INDEX; do
         cat $FORMAT_FILE | jq ".[$f].name"
-        local readonly REGEXP=$(cat $FORMAT_FILE | jq ".[$f].wallet_regexp")
+        local readonly REGEXP=$(cat $FORMAT_FILE | jq --raw-output ".[$f].wallet_regexp")
+        echo $REGEXP
+        sleep 1
         local readonly SYMBOL=$(cat $FORMAT_FILE | jq --raw-output ".[$f].symbol")
-        
+
         local readonly RESULT_FILE="$BASE_RESULT_FILE-$SYMBOL.json"
-        local readonly SEARCH_STRING="$SYMBOL+donation"
-        
+        local readonly SEARCH_STRING="$SYMBOL+Donation"
+
         # 0..49 are the allowed result page for the searchcode api
         for p in {0..49}; do
+            echo "page $p"
             local readonly OUTPUT_FILE="$BASE_OUTPUT_FILE-$SYMBOL-$p.json"
             local readonly RESULT_FILE="$BASE_RESULT_FILE-$SYMBOL-$p.json"
-            
+
             # Download the json file containing the infos
-            curl -s -o $OUTPUT_FILE https://searchcode.com/api/codesearch_I/?q=$SEARCH_STRING&p=$p&per_page=100&loc=5
-            
+            curl -s -o $OUTPUT_FILE https://searchcode.com/api/codesearch_I/?q=${SEARCH_STRING}&p=$p&per_page=100&loc=1000
             #~ # Extract the "result" key from the json file and store it in $TMP_RESULT_FILE
             cat $OUTPUT_FILE | jq '.' |  jq '.["results"]' > $RESULT_FILE
-            
+
             #~ # Extract the indices of the result_file
             local readonly KEYS=$(cat $RESULT_FILE | jq 'keys' | jq '.[]')
-            
+
             for k in $KEYS; do
                 local readonly TMP=$(cat $RESULT_FILE | jq ".[$k]")
-                
-                
-                
-                local readonly MATCH=$(echo $TMP | jq '.' | egrep -o "$REGEXP")
+                local readonly MATCH=$(echo $TMP | jq "." | jq ".lines" | egrep -o "$REGEXP")
                 #~ echo $TMP | jq '.' | egrep -o "$REGEXP"
                 if [ "$MATCH" != "" ]; then # a match is found
-                    echo "==="
-                    echo $TMP | jq "."
+                    echo "====="
+                    #echo $TMP | jq "."
                     echo $MATCH
                     local readonly REPOSITORY=$(echo $TMP | jq --raw-output ".repo" | sed 's/\.[^.]*$//')
                     local readonly RAW_REPOSITORY=$(echo $REPOSITORY | sed 's/github.com/raw.githubusercontent.com/')
@@ -51,8 +51,10 @@ main() {
                     else
                         URL=$RAW_REPOSITORY/master/$DIRECTORY/$FILENAME
                     fi
-                    echo $URL
-                    echo "==="
+                    echo "$URL $counter"
+
+                    counter=$((counter+1))
+                    echo "====="
                 fi
             done
         done
