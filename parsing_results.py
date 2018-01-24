64c5741c1d7e38b9583bb079bb2074c9253b517d
mzanella <zanna0150@gmail.com>
2018-01-24 13:49:10 +0100

    start parsing

---
+++ parsing_results.py
@@ -0,0 +1,12 @@
+import json
+
+
+class Parser:
+    def parse(path):
+        results = json.load(open(path))
+        noDuplicate = []
+        for res in results["results"]:
+            if res not in noDuplicate:
+                noDuplicate.append(res)
+        for res in noDuplicate:
+            print(res["+username"])
