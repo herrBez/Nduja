16fe1d22f69c788fde3edaff89d951db2be91b2c
mzanella <zanna0150@gmail.com>
2018-01-28 23:22:28 +0100

    Search on multithread?

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 from user_info_retriever.info_retriever import InfoRetriever
 from wallet_collectors.github_wallet_collector import GithubWalletCollector
-from threading import Thread
+from multiprocessing import Pool
 
 
 def main():
@@ -17,17 +17,15 @@ def main():
     else:
         config.read('./Nduja/default-conf.ini')
     DbManager.setDBFileName(config.get('file_names', 'dbname'))
+    pool = Pool(processes=3)
+    p1 = pool.apply_async(searchGithub, [])
+    p2 = pool.apply_async(searchTwitter, [])
+    # c = pool.apply_async(printNum3, [])
+    pool.close()
+    pool.join()
     # t1 = \
     #     Thread(target=searchSearchCode(
     #         config.get('file_names', 'result_file')))
-    # t1.start()
-    # t1.join()
-    t2 = Thread(target=searchGithub)
-    t3 = Thread(target=searchTwitter)
-    t2.start()
-    t3.start()
-    t2.join()
-    t3.join()
     try:
         tokens = {'github': config.get('tokens', 'github'),
                   'twitter_app_key': config.get('tokens', 'twitter_app_key'),
