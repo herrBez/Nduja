3d64300570ac8979164ab87fcd9f1317deb27119
herrBez <mirko.bez@studenti.unipd.it>
2018-01-29 22:30:12 +0100

    Use async requests for collecting the raw results using the python module grequests

--- wallet_collectors/searchcode_wallet_collector.py
+++ wallet_collectors/searchcode_wallet_collector.py
@@ -2,10 +2,14 @@ import json
 from wallet_collectors.abs_wallet_collector import AbsWalletCollector
 import re
 from time import sleep
+import grequests
+import requests
 
 def print_json(s):
     print(json.dumps(s, indent=2))
 
+def exception_handler(request, exception):
+        print(exception)
 
 class SearchcodeWalletCollector(AbsWalletCollector):
 
@@ -14,30 +18,32 @@ class SearchcodeWalletCollector(AbsWalletCollector):
         self.max_page = 10
         self.per_page = 10
 
-    def collect_raw_result(self, query):
-        tmp = json.loads(
-            self.request_url(
-                query
-            )
-        )
-        return tmp["results"]
-
-    def construct_queries(self, pattern) -> list:
-        pages = range(0, self.max_page)
-        return list(
-            map(lambda page:
-                "https://searchcode.com/api/codesearch_I/?"
-                + "q="
-                + pattern.symbol
-                + "+Donation"
-                + "&p="
-                + str(page)
-                + "&per_page"
-                + str(self.per_page)
-                + "&loc=0",
-                pages
+    def collect_raw_result(self, queries):
+        rs = (grequests.get(q) for q in queries)
+        raw_results = grequests.imap(rs, exception_handler=exception_handler)
+        raw_results = list(
+            map(lambda r:
+                r.json()["results"],
+                raw_results
                 )
         )
+
+        return raw_results
+
+    def construct_queries(self) -> list:
+        return [
+            "https://searchcode.com/api/codesearch_I/?"
+            + "q="
+            + pattern.symbol
+            + "+Donation"
+            + "&p="
+            + str(page)
+            + "&per_page"
+            + str(self.per_page)
+            + "&loc=0"
+            for pattern in self.patterns
+            for page in range(0, self.max_page)
+        ]
 
     def extract_content(self, response) -> str:
         res = ""
@@ -49,21 +55,21 @@ class SearchcodeWalletCollector(AbsWalletCollector):
     def build_answer_json(self, item, content, symbol_list, wallet_list):
         repo = item["repo"]
         username_pattern = re.compile("(https?|git)://([^/]*)/([^/]*)/([^/]*)")
-        mymatch = username_pattern.search(repo)
+        my_match = username_pattern.search(repo)
 
 
         if "bitbucket" in repo:
             hostname = "bitbucket.org"
-            username = mymatch.group(4)
+            username = my_match.group(4)
         elif "github" in repo:
             hostname = "github.com"
-            username = mymatch.group(3)
+            username = my_match.group(3)
         elif "google.code" in repo:
             hostname = "google.code.com"
-            username = mymatch.group(3)
+            username = my_match.group(3)
         elif "gitlab" in repo:
             hostname = "gitlab.com"
-            username = mymatch.group(3)
+            username = my_match.group(3)
         else:
             # Not known source
             hostname = ""
