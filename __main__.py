905412ce4825b137bb079c4948c949bc04b35e54
herrBez <mirko.bez@studenti.unipd.it>
2018-02-05 11:28:54 +0100

    Use thread pool executor in main and comment out information retrieve

--- __main__.py
+++ __main__.py
@@ -7,18 +7,22 @@ from wallet_collectors.searchcode_wallet_collector import SearchcodeWalletCollec
 import sys
 from user_info_retriever.info_retriever import InfoRetriever
 from wallet_collectors.github_wallet_collector import GithubWalletCollector
-from multiprocessing import Pool
+from multiprocessing.dummy import Pool
+# from multiprocessing.pool import ThreadPool
+from concurrent.futures import  ThreadPoolExecutor
 import json
 import twython
 from address_checkers.eth_address_checker import EthAddressChecker
 from time import sleep
 import logging
+from sys import argv
 
 def search_searchcode(formatfile):
     logging.info("Search Code")
     results = (SearchcodeWalletCollector(formatfile)
                .collect_address())
     Parser().parseString(results)
+    return "ok searchcode"
 
 
 def search_github(formatfile, tokens):
@@ -29,7 +33,7 @@ def search_github(formatfile, tokens):
                .collect_address())
     logging.info("Finish Search Github")
     Parser().parseString(results)
-
+    return "ok search_github"
 
 def search_twitter(formatfile, tokens):
     try:
@@ -42,9 +46,16 @@ def search_twitter(formatfile, tokens):
     except twython.exceptions.TwythonRateLimitError:
         logging.error("Twython rate limit exceed!. Exiting without crash")
 
+    return "ok search_twitter"
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
+
+    # if len(sys.argv) < 2:
+    #     logging.error("python3 %s <search_type>" % sys.argv[0])
+    #     sys.exit(0)
+    # search_type = sys.argv[1]
+
 
     logging.basicConfig(level=logging.INFO)
 
@@ -57,22 +68,36 @@ if __name__ == "__main__":
 
     DbManager.setDBFileName(config["dbname"])
     EthAddressChecker.setToken(config["tokens"]["etherscan"])
-    pool = Pool(processes=3)
+    # pool = Pool(processes=3)
 
-    #p1 = pool.apply_async(search_github(config["format"],
-    #                                    config["tokens"]["github"]), [])
-    p2 = pool.apply_async(search_twitter(config["format"],
-                                         config["tokens"]), [])
+    executor = ThreadPoolExecutor(max_workers=4)
+    f1 = executor.submit(search_github,config["format"],
+                                       config["tokens"]["github"])
+
+    f2 = executor.submit(search_twitter, config["format"],
+                                         config["tokens"])
+    f3 = executor.submit(search_searchcode, config["format"])
+
+    print("Let's wait bitch")
+
+    print(f1.result())
+    print(f2.result())
+    print(f3.result())
+
+
+    # p1 = pool.apply_async(search_github(config["format"],
+    #                                     config["tokens"]["github"]), [])
+    # p2 = pool.apply_async(search_twitter(config["format"],
+    #                                      config["tokens"]), [])
     # p3 = pool.apply_async(search_searchcode(config["format"]), [])
-
-
-    pool.close()
-    pool.join()
-    try:
-        InfoRetriever.setTokens(config)
-    except KeyError:
-        print()
-    logging.info("Finish to fetch the data. Sleep 15 minutes to let the api ")
-    # sleep(15*62)
-    logging.info("Finish the sleep for the twitter api")
-    # InfoRetriever().retrieveInfoForAccountSaved()
+    #
+    #
+    # pool.join()
+    # try:
+    #     InfoRetriever.setTokens(config)
+    # except KeyError:
+    #     print()
+    # logging.info("Finish to fetch the data. Sleep 15 minutes to let the api ")
+    # # sleep(15*62)
+    # logging.info("Finish the sleep for the twitter api")
+    # # InfoRetriever().retrieveInfoForAccountSaved()
