5d78066532b9eb60d1852076ec8909c4218d709d
herrBez <bez.mirko@gmail.com>
2018-02-13 18:14:43 +0100

    Fix the definitive structure of the Cluster Class

--- Nduja/graph/Cluster.py
+++ Nduja/graph/Cluster.py
@@ -22,8 +22,24 @@ class Cluster:
     def inferred_addresses(self) -> Iterable[Wallet]:
         return self._inferred_addresses
 
+    def add_inffered_address(self, address: Wallet):
+        self._inferred_addresses.add(address)
+
+    def merge_original_list(self, other: Cluster_Type):
+        self.original_address.update(other.original_address)
+
     def merge(self, other: Cluster_Type) -> Cluster_Type:
         originals = self.original_address.union(other.original_address)
         inferred = self.inferred_addresses.union(other.inferred_addresses)
         return Cluster(originals, inferred)
 
+    def __hash__(self):
+        return hash(frozenset(self._inferred_addresses))
+
+    def __eq__(self, other) -> bool:
+        return type(self) == type(other) and \
+               self._inferred_addresses == other.inferred_addresses
+
+    def __str__(self) -> str:
+        return str(list(self._original_addresses)) + "\n" + \
+               str(list(self._inferred_addresses))
