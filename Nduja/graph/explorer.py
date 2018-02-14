9842cfb8c49a2f9a26fe9e640cbee1eb36f35c8f
herrBez <bez.mirko@gmail.com>
2018-02-14 17:04:25 +0100

    Add explorer

--- Nduja/graph/explorer.py
+++ Nduja/graph/explorer.py
@@ -1,66 +1,125 @@
 from time import sleep
 from typing import List
+from typing import Set
 from typing import Tuple
 
 from dao.wallet import Wallet
 from graph.BitcoinTransactionRetriever import BtcTransactionRetriever
 from db.db_manager import DbManager
-from graph.graph_builder import CurrencyGraph
+# from graph.graph_builder import CurrencyGraph
+from graph.cluster import Cluster
+from graph.cluster_graph import ClusterGraph
 
 
-# Performing a Breadth First Search
-def explore(start_addresses: List[Wallet], black_list_address: List[Wallet],
-            max_hop: int = 1) -> None:
-    btr = BtcTransactionRetriever()
+# # Performing a Breadth First Search
+# def explore(start_addresses: List[Wallet], black_list_address: List[Wallet],
+#             max_hop: int = 1) -> None:
+#     btr = BtcTransactionRetriever()
+#
+#     stack = []  # type: List[Tuple[Wallet, int]]
+#
+#     g = CurrencyGraph(start_addresses)
+#     for b in black_list_address:
+#         g.add_node(b)
+#
+#     print(start_addresses)
+#
+#     # avoid duplicates
+#     addresses = list(set(start_addresses))
+#     print(addresses)
+#     for address in addresses:
+#         stack.append((address, 0))
+#
+#     already_explored_addresses = set(addresses + black_list_address)
+#     it = 0
+#
+#     while len(stack) > 0:
+#         address, hop = stack.pop(0)
+#         print(str(len(stack)) + ":" + str(hop))
+#         print(str(it) + "/" + str(len(start_addresses)))
+#         it += 1
+#
+#         # For now exit if hop is 2
+#         if hop > max_hop:
+#             break
+#
+#         already_explored_addresses.add(address)
+#
+#         in_dict, out_dict, connected_addresses \
+#             = btr.get_input_output_addresses(address)
+#
+#         for i in in_dict:
+#             if i not in already_explored_addresses and i not in stack:
+#                 stack.append((i, hop+1))
+#                 g.add_node(i)
+#                 g.add_str_edge(i, address, weight=in_dict[i])
+#
+#         for o in out_dict:
+#             if o not in already_explored_addresses and o not in stack:
+#                 stack.append((o, hop+1))
+#                 g.add_node(o)
+#                 g.add_str_edge(address, o, weight=out_dict[o])
+#
+#         for o in connected_addresses:
+#             print(o + " is connected with " + address)
+#
+#     g.plot(blacklist=black_list_address)
 
-    stack = []  # type: List[Tuple[Wallet, int]]
 
-    g = CurrencyGraph(start_addresses)
-    for b in black_list_address:
-        g.add_node(b)
+def fill_clusters(clusters, black_list=[]):
+    btc_transaction_retriever = BtcTransactionRetriever()
 
-    print(start_addresses)
+    clusters_length = len(clusters)
+    processed = 0
 
-    # avoid duplicates
-    addresses = list(set(start_addresses))
-    print(addresses)
-    for address in addresses:
-        stack.append((address, 0))
+    for c in clusters:
+        progress = 100*processed/clusters_length
 
-    already_explored_addresses = set(addresses + black_list_address)
-    it = 0
+        print("\033[K" + f"processed\t {processed:d} / {clusters_length:d}\t"
+              + f"{progress:.2f}%\r", sep=' ', end='', flush=True)
+        processed += 1
+        stack = set([])
+        tmp_black_list = []
+        for saddr in c.inferred_addresses:
+            stack.add(saddr)
 
-    while len(stack) > 0:
-        address, hop = stack.pop(0)
-        print(str(len(stack)) + ":" + str(hop))
-        print(str(it) + "/" + str(len(start_addresses)))
-        it += 1
+        while len(stack) > 0:
+            elem = stack.pop()
+            c.add_inferred_address(elem)
+            tmp_black_list.append(elem)
+            a, b, siblings = btc_transaction_retriever.\
+                get_input_output_addresses(elem.address)
+            for s in siblings:
+                w = Wallet(s, "BTC", "", 1, True)
+                if w not in black_list and w not in tmp_black_list:
+                    stack.add(w)
+    return clusters
 
-        # For now exit if hop is 2
-        if hop > max_hop:
-            break
 
-        already_explored_addresses.add(address)
 
-        in_dict, out_dict, connected_addresses \
-            = btr.get_input_output_addresses(address)
+def explore_output(clusters: List[Cluster]):
+    btc_transaction_retriever = BtcTransactionRetriever()
+    for c in clusters:
+        output_lists = {}
+        output_cluster_list = []
+        for w in c.inferred_addresses:
+            a, output, siblings = btc_transaction_retriever.get_input_output_addresses(w.address)
+            for k in output:  # Take the keys, i.e. the addresses
+                output_cluster_list.append(Cluster(Wallet(k, "BTC", "", 1, True)))
+                fill_clusters(Cluster)
 
-        for i in in_dict:
-            if i not in already_explored_addresses and i not in stack:
-                stack.append((i, hop+1))
-                g.add_node(i)
-                g.add_str_edge(i, address, weight=in_dict[i])
 
-        for o in out_dict:
-            if o not in already_explored_addresses and o not in stack:
-                stack.append((o, hop+1))
-                g.add_node(o)
-                g.add_str_edge(address, o, weight=out_dict[o])
 
-        for o in connected_addresses:
-            print(o + " is connected with " + address)
+def merge_clusters(clusters: List[Cluster]) -> Set[Cluster]:
 
-    g.plot(blacklist=black_list_address)
+    clusters_set = list(set(clusters))
+
+    for c in clusters:
+        for d in clusters_set:
+            if c == d:
+                d.merge_original_list(c)
+    return clusters_set
+
 
 
 def main() -> None:
@@ -71,18 +130,70 @@ def main() -> None:
 
     db.initConnection()
 
-    black_list = db.getAllKnownWallets()
+    black_list = [w for w in db.getAllKnownWallets()]
 
-    addresses = [w for w in db.getAllWalletsByCurrency("BTC") if
-                 w.address not in black_list]
+    clusters = [Cluster([w]) for w in db.getAllWalletsByCurrency("BTC")[3:4] if
+                w not in black_list]
+
+
+    cluster_black_list = [Cluster([w]) for w in black_list]
+
+
+    print()
+    print()
+    print()
 
     db.closeDb()
 
-    if len(addresses) < 1:
-        addresses = ["17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j",
-                     "1CXEo9yJwU5V3d6FmGyt6ni8KFE26i6t8i"]
+    fill_clusters(cluster_black_list, black_list=[])
 
-    explore(addresses, black_list, max_hop=0)
+    clusters_set = merge_clusters(clusters)
+
+    print(f"Before {len(clusters):d}: After {len(clusters_set)}")
+
+    # for c in clusters_set:
+    #     print("===")
+    #     for c1 in c.inferred_addresses:
+    #         print(c1)
+    #     print("===")
+
+    clusters = list(clusters_set)
+    # explore_output(clusters)
+
+    # explore(clusters, black_list=black_list, max_hop=1)
+
+    # explore(addresses, black_list, max_hop=0)
+
+def main2():
+    DbManager.setDBFileName("nduja.db")
+
+    db = DbManager.getInstance()
+
+    db.initConnection()
+
+    black_list = [w for w in db.getAllKnownWallets()]
+
+    clusters = [Cluster([w]) for w in db.getAllWalletsByCurrency("BTC")[3:5] if
+                w not in black_list]
+
+    cluster_black_list = [Cluster([w]) for w in black_list]
+
+    print()
+    print()
+    print()
+
+    db.closeDb()
+
+    fill_clusters(clusters, black_list=[])
+
+    clusters_set = merge_clusters(clusters)
+
+    clusters = list(clusters_set)
+
+    graph = ClusterGraph(clusters_set)
+
+    graph.plot()
+    print("Done")
 
 
-main()
+main2()
