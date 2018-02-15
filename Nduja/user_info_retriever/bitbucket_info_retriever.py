39410a619d6a9497b02e722589017802f701caf6
mzanella <zanna0150@gmail.com>
2018-02-15 09:47:34 +0100

    Make some method of BitbucketInfoRetriever static

--- Nduja/user_info_retriever/bitbucket_info_retriever.py
+++ Nduja/user_info_retriever/bitbucket_info_retriever.py
@@ -11,16 +11,19 @@ class BitbucketInfoRetriever(PersonalInfoRetriever):
     URL = "https://api.bitbucket.org/2.0/users/"
 
     def retrieve_info_from_account(self, account: Account) -> PersonalInfo:
-        response = requests.get(self.format_url(account.username))
-        return self.parse_result(response)
+        response = requests.get(BitbucketInfoRetriever.
+                                format_url(account.username))
+        return BitbucketInfoRetriever.parse_result(response)
 
-    def format_url(self, username: str) -> str:
+    @staticmethod
+    def format_url(username: str) -> str:
         if username is None or username.isspace():
             return None
 
         return BitbucketInfoRetriever.URL + username
 
-    def parse_result(self, response: Response) -> PersonalInfo:
+    @staticmethod
+    def parse_result(response: Response) -> PersonalInfo:
         info = None
         if response is not None:
             info = PersonalInfo(response.json()["display_name"],
