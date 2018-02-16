a8c24b45758c95434c52549d3c59eabebc7717dd
mzanella <zanna0150@gmail.com>
2018-02-16 14:19:50 +0100

    Remove pattern class and utilities from abs_wallet_collector and put them in utility/pattern
    + start other info retrieving from file of addresses

---
+++ Nduja/user_info_retriever/other_info_retriever.py
@@ -0,0 +1,81 @@
+import traceback
+
+from urlparse import urlparse
+from utility.pattern import *
+import sqlite3
+import requests
+import os
+import time
+
+
+def uri_validator(x):
+    try:
+        result = urlparse(x)
+        return result.scheme and result.netloc and result.path
+    except:
+        return False
+
+
+def other_info_retriever(db_name: str):
+    path = os.path.dirname(os.path.abspath(__file__))
+    db_conn = sqlite3.connect(path + '/nduja.db')
+    db_cur = db_conn.cursor()
+    db_cur.execute('''SELECT Account, RawURL FROM AccountWallet''')
+    account_email_dict = {}  # type: Dict[int, str]
+    account_website_dict = {}  # type: Dict[int, str]
+    for row in db_cur:
+        try:
+            resp_text = None
+            if row[1] is not None and uri_validator(row[1]):
+                try:
+                    response = requests.get(row[1])
+                    resp_text = response.text
+                except:
+                    traceback.print_exc()
+            if resp_text is not None:
+                email_list = match_email(resp_text)
+                website_list = match_personal_website(resp_text)
+                # print(str(row[0]) + " " + str(email_list) + " " + str(website_list))
+                time.sleep(1)
+                emails = ''
+                if len(email_list) == 1:
+                    emails = email_list[0]
+                elif 1 < len(email_list) < 6:
+                    emails = ','.join(email_list)
+                websites = ''
+                if len(website_list) == 1:
+                    websites = website_list[0]
+                elif 1 < len(website_list) < 9:
+                    websites = ','.join(website_list)
+                print(str(row[0]) + ' emails:' + str(len(email_list)) +
+                      ' websites:' + str(len(website_list)))
+                account_email_dict[row[0]] = emails
+                account_website_dict[row[0]] = websites
+        except:
+            traceback.print_exc()
+    db_cur.execute('''PRAGMA foreign_keys = OFF''')
+    for id_acc in account_email_dict:
+        db_cur.execute('''SELECT Info FROM Account WHERE _id = ?''', (id_acc,))
+        info = db_cur.fetchone()[0]
+        if isinstance(info, int):
+            db_cur.execute('''SELECT * FROM Info WHERE _id = ?''', (info,))
+            row = db_cur.fetchone()
+            websites = ','.join([row[2], account_website_dict[id_acc]])
+            emails = ','.join([row[3], account_email_dict[id_acc]])
+            db_cur.execute('''UPDATE Information
+                              SET Website = ?, Email = ?
+                              WHERE _id = ?''', (websites, emails, info,))
+        else:
+            emails = account_email_dict[id_acc]
+            websites = account_website_dict[id_acc]
+            db_cur.execute('''INSERT INTO 
+                              Information(Name, Website, Email, Json)
+                              VALUES (?,?,?,?)''',
+                           (' ', websites, emails, ' ',))
+            db_cur.execute('SELECT max(_id) FROM Information')
+            max_id = db_cur.fetchone()[0]
+            db_cur.execute('''UPDATE Account
+                              SET Info = ?
+                              WHERE _id = ?''', (max_id, id_acc))
+    db_conn.commit()
+    db_conn.close()
---
+++ Nduja/utility/pattern.py
@@ -0,0 +1,65 @@
+import re
+from typing import List, Tuple, Dict
+
+
+class Pattern:
+
+    def __init__(self, format_object: Dict[str, str]) -> None:
+        self._pattern = re.compile(format_object["wallet_regexp"])
+        self._name = format_object["name"]
+        self._group = format_object["group"]
+        self._symbol = format_object["symbol"]
+
+    @property
+    def pattern(self):
+        return self._pattern
+
+    @pattern.setter
+    def pattern(self, value):
+        self._pattern = value
+
+    @property
+    def symbol(self) -> str: return self._symbol
+
+    @symbol.setter
+    def symbol(self, value: str):
+        self._symbol = value
+
+    @property
+    def name(self) -> str: return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    def __str__(self) -> str:
+        return self.symbol + " Pattern "
+
+    def match(self, content: str) -> List[Tuple[str, str]]:
+        matches_iterator = self.pattern.finditer(content)
+        matches = list(
+            map(
+                lambda x:
+                (self.symbol, x.group()),
+                matches_iterator
+            )
+        )
+        return matches
+
+
+def match_email(text: str) -> List[str]:
+    """Check if inside the text there is a list of emails"""
+    pattern = re.compile(
+        "\\b([a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)\\b")
+    emails = pattern.findall(text)
+    return emails
+
+
+def match_personal_website(text: str) -> List[str]:
+    """Check if inside the given text there is a list of websites"""
+    pattern = re.compile("\\b(https?://([^/\\s]+/?)*)\\b")
+    website_matches = pattern.findall(text)
+    # Filter out all results that links to license reference
+    website_matches = [w[0] for w in website_matches if
+                       "license" not in w[0]]
+    return website_matches
