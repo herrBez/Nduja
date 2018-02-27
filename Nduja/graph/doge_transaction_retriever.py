d870b227767dcf32b74c84b08dd58c6c124712d1
mzanella <zanna0150@gmail.com>
2018-02-27 08:27:00 +0100

    Fix super constructor call ltc and doge retriever

--- Nduja/graph/doge_transaction_retriever.py
+++ Nduja/graph/doge_transaction_retriever.py
@@ -4,4 +4,4 @@ from graph.chainso_transaction_retriever import ChainSoTransactionRetriever
 class DogeTransactionRetriever(ChainSoTransactionRetriever):
 
     def __init__(self):
-        super(self, "DOGE").__init__()
+        super().__init__("DOGE")
--- Nduja/graph/ltc_transaction_retriever.py
+++ Nduja/graph/ltc_transaction_retriever.py
@@ -4,4 +4,4 @@ from graph.chainso_transaction_retriever import ChainSoTransactionRetriever
 class LtcTransactionRetriever(ChainSoTransactionRetriever):
 
     def __init__(self):
-        super(self, "LTC").__init__()
+        super().__init__("LTC")
