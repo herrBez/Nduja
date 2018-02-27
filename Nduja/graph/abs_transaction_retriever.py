7cec16a2b4e70948adc039958f757d208ebd7ce4
mzanella <zanna0150@gmail.com>
2018-02-27 07:44:03 +0100

    Add parent class for all retrievers

---
+++ Nduja/graph/abs_transaction_retriever.py
@@ -0,0 +1,21 @@
+from abc import ABCMeta, abstractmethod
+from typing import Optional, Dict, Tuple
+import time
+
+
+def get_epoch() -> int:
+    return int(time.time())
+
+
+class AbsTransactionRetriever:
+    """Abstract base class for transaction retrievers"""
+    __metaclass__ = ABCMeta
+    timestamp_class = None  # type: Optional[int]
+
+    @abstractmethod
+    def get_input_output_addresses(self, address: str,
+                                   timestamp: Optional[int] = None) -> \
+            Tuple[Dict[str, int],  Dict[str, int], Dict[str, int]]:
+        """Given an address it returns ALL transactions performed
+        by the address"""
+        pass
