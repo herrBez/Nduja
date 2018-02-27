b01fbcc35b38201c2b1cf667ca4c0fbe3a88fa7a
mzanella <zanna0150@gmail.com>
2018-02-27 07:51:04 +0100

    Comment main() and make main2() parametric on currency and db name and change cluster creation according to new modification

--- Nduja/graph/explorer.py
+++ Nduja/graph/explorer.py
@@ -5,9 +5,12 @@ from typing import Tuple
 from typing import Dict
 from typing import Any
 import time
+
+import sys
 from dao.wallet import Wallet
-from graph.BitcoinTransactionRetriever import BtcTransactionRetriever
-from graph.ChainSoTransactionRetriever import ChainSoTransactionRetriever
+from graph.abs_transaction_retriever import AbsTransactionRetriever
+from graph.bitcoin_transaction_retriever import BtcTransactionRetriever
+from graph.chainso_transaction_retriever import ChainSoTransactionRetriever
 from db.db_manager import DbManager
 # from graph.graph_builder import CurrencyGraph
 from graph.cluster import Cluster
@@ -68,6 +71,9 @@ from graph.cluster_graph import ClusterGraph
 #             print(o + " is connected with " + address)
 #
 #     g.plot(blacklist=black_list_address)
+from graph.doge_transaction_retriever import DogeTransactionRetriever
+from graph.etherscan_transaction_retriever import EtherscanTransactionRetriever
+from graph.ltc_transaction_retriever import LtcTransactionRetriever
 
 
 def fill_clusters(clusters, black_list=[]):
@@ -100,18 +106,18 @@ def fill_clusters(clusters, black_list=[]):
     return clusters
 
 
-
-def explore_output(clusters: List[Cluster]):
-    btc_transaction_retriever = BtcTransactionRetriever()
+def explore_output(clusters: List[Cluster],
+                   transaction_retriever: AbsTransactionRetriever):
     for c in clusters:
         output_lists = {} # type: Dict[str, Any]
         output_cluster_list = []
         for w in c.inferred_addresses:
-            a, output, siblings = btc_transaction_retriever.get_input_output_addresses(w.address)
+            a, output, siblings = transaction_retriever.\
+                get_input_output_addresses(w.address)
             for k in output:  # Take the keys, i.e. the addresses
-                output_cluster_list.append(Cluster([Wallet(k, "BTC", "", 1, True)]))
+                output_cluster_list.append(Cluster(
+                    [Wallet(k, "BTC", "", 1, True)], transaction_retriever))
                 fill_clusters(Cluster)
-
 
 
 def merge_clusters(clusters: List[Cluster]) -> Set[Cluster]:
@@ -125,48 +131,47 @@ def merge_clusters(clusters: List[Cluster]) -> Set[Cluster]:
     return clusters_set
 
 
+# def main() -> None:
 
-def main() -> None:
-
-    DbManager.set_db_file_name("nduja.db")
-
-    db = DbManager.get_instance()
-
-    db.init_connection()
-
-    black_list = [w for w in db.get_all_known_wallets()]
-
-    clusters = [Cluster([w]) for w
-                in db.get_all_wallets_by_currency("BTC")[3:4] if
-                w not in black_list]
-
-
-    cluster_black_list = [Cluster([w]) for w in black_list]
-
-
-    print()
-    print()
-    print()
-
-    db.close_db()
-
-    fill_clusters(cluster_black_list, black_list=[])
-
-    clusters_set = merge_clusters(clusters)
-
+    # DbManager.set_db_file_name("nduja.db")
+    #
+    # db = DbManager.get_instance()
+    #
+    # db.init_connection()
+    #
+    # black_list = [w for w in db.get_all_known_wallets()]
+    #
+    # clusters = [Cluster([w], ) for w
+    #             in db.get_all_wallets_by_currency("BTC")[3:4] if
+    #             w not in black_list]
+    #
+    #
+    # cluster_black_list = [Cluster([w]) for w in black_list]
+    #
+    #
+    # print()
+    # print()
+    # print()
+    #
+    # db.close_db()
+    #
+    # fill_clusters(cluster_black_list, black_list=[])
+    #
+    # clusters_set = merge_clusters(clusters)
+    #
     # print(f"Before {len(clusters):d}: After {len(clusters_set)}")
-
+    #
     # for c in clusters_set:
     #     print("===")
     #     for c1 in c.inferred_addresses:
     #         print(c1)
     #     print("===")
-
-    clusters = list(clusters_set)
+    #
+    # clusters = list(clusters_set)
     # explore_output(clusters)
-
+    #
     # explore(clusters, black_list=black_list, max_hop=1)
-
+    #
     # explore(addresses, black_list, max_hop=0)
 
 
@@ -174,25 +179,40 @@ def get_epoch() -> int:
     return int(time.time())
 
 
+def get_retriever_by_currency(currency: str) -> AbsTransactionRetriever:
+    return {
+        "BTC":  BtcTransactionRetriever(),
+        "LTC":  LtcTransactionRetriever(),
+        "DOGE": DogeTransactionRetriever(),
+        "ETH":  EtherscanTransactionRetriever()
+    }[currency]
+
+
 def main2() -> None:
-    DbManager.set_db_file_name("nduja_cleaned_wc.db")
-
-    epoch = get_epoch()
-
-    BtcTransactionRetriever.timestamp_class = epoch
-    ChainSoTransactionRetriever.timestamp_class = epoch
-
+    """
+        Requires 2 arguments:
+         - first -> db name
+         - second -> currency (BTC, ETH, LTC, DOGE)
+    """
+    DbManager.set_db_file_name(sys.argv[1])
     db = DbManager.get_instance()
-
     db.init_connection()
 
-    black_list = [w for w in db.get_all_known_wallets_by_currency("BTC")]
+    currency = sys.argv[2] if sys.argv[2] in ["BTC", "ETH", "LTC", "DOGE"] \
+        else "BTC"
 
-    clusters = [Cluster([w], [], db.find_accounts_by_wallet(w))
-                for w in db.get_all_wallets_by_currency("BTC")
+    epoch = get_epoch()
+    AbsTransactionRetriever.timestamp_class = epoch
+    transaction_retriever = get_retriever_by_currency(currency)
+
+    black_list = [w for w in db.get_all_known_wallets_by_currency(currency)]
+
+    clusters = [Cluster([w], transaction_retriever, [],
+                        db.find_accounts_by_wallet(w))
+                for w in db.get_all_wallets_by_currency(currency)
                 if w not in black_list]
 
-    black_list_cluster = Cluster(black_list, [], [99999])
+    black_list_cluster = Cluster(black_list, transaction_retriever, [], [99999])
 
     db.close_db()
     print("qui ci siamo")
@@ -202,20 +222,14 @@ def main2() -> None:
         print(list(cluster.inferred_addresses))
     print("Filled")
 
-
     clusters = [cluster for cluster in clusters
                 if not cluster.belongsToBlackList]
-
     clusters_set = set(clusters)
-
     clusters = list(clusters_set)
 
     db.init_connection()
-
     db.insert_clusters(clusters_set)
-
     db.save_changes()
-
     db.close_db()
 
 #    graph = ClusterGraph(list(clusters_set), black_list_cluster)
